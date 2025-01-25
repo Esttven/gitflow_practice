7db8d7ea4bbff1157705a12e8660bbd977740adf
Esteban Arteaga <mattesteban@outlook.es>
2025-01-25 10:49:41 -0500

    change background color darkcyan

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div style={{ backgroundColor: '#f0f0f0' }}>
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
